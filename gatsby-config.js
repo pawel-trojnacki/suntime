d965e9b474399175058f74965d1ed1cb5805351d
Pawel Trojnacki <trojnacki.p@outlook.com>
2020-08-05 23:51:49 +0200

    Change developMiddleweare to proxy option

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
-const { createProxyMiddleware } = require('http-proxy-middleware'); //v1.x.x
+// const { createProxyMiddleware } = require('http-proxy-middleware'); //v1.x.x
 // Use implicit require for v0.x.x of 'http-proxy-middleware'
 // const proxy = require('http-proxy-middleware')
 // be sure to replace 'createProxyMiddleware' with 'proxy' where applicable
@@ -14,18 +14,21 @@ module.exports = {
     author: `Paweł Trojnacki`,
     siteUrl: `https://suntime.netlify.app/`,
   },
-  developMiddleware: app => {
-    app.use(
-      '/.netlify/functions/',
-      createProxyMiddleware({
-        target: 'http://localhost:8000',
-        secure: false, // Do not reject self-signed certificates.
-        pathRewrite: {
-          '/.netlify/functions/': '',
-        },
-      })
-    );
+  proxy: {
+    url: 'https://suntime.netlify.app/',
   },
+  // developMiddleware: app => {
+  //   app.use(
+  //     '/.netlify/functions/',
+  //     createProxyMiddleware({
+  //       target: 'http://localhost:8000',
+  //       secure: false, // Do not reject self-signed certificates.
+  //       pathRewrite: {
+  //         '/.netlify/functions/': '',
+  //       },
+  //     })
+  //   );
+  // },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
