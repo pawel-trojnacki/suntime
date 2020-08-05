aa5ee2242b645d36476709a3fa263bba4bb06e2f
Pawel Trojnacki <trojnacki.p@outlook.com>
2020-08-05 23:40:11 +0200

    Add createProxyMiddleweare

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,11 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
+const { createProxyMiddleware } = require('http-proxy-middleware'); //v1.x.x
+// Use implicit require for v0.x.x of 'http-proxy-middleware'
+// const proxy = require('http-proxy-middleware')
+// be sure to replace 'createProxyMiddleware' with 'proxy' where applicable
+
 module.exports = {
   siteMetadata: {
     title: `Suntime`,
